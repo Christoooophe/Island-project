85fe9df75dcaf264c739aaa44acc18a19aa0a109
Christophe <christophealfier02@gmail.com>
2023-01-30 11:04:39 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+
+function changerSlide() {
+    document.body.style.backgroundImage = "url('./assets/pexels-james-wheeler-417074.jpg')";
+    document.getElementById("parent").style.backgroundImage = "linear-gradient(270deg, rgba(0,0,0,0.073564459963673) 31%, rgba(0, 0, 0, 0.247) 54%, rgba(0, 0, 0, 0.411) 100%),url('./assets/pexels-james-wheeler-417074.jpg')";
+    document.getElementById("parent").style.background = "background-size: cover;";
+};
+function changerSlideTwo() {
+    document.body.style.backgroundImage = "url('./assets/pexels-pixabay-355321.jpg')";
+    document.getElementById("parent").style.backgroundImage = "linear-gradient(270deg, rgba(0,0,0,0.073564459963673) 31%, rgba(0, 0, 0, 0.247) 54%, rgba(0, 0, 0, 0.411) 100%),url('./assets/pexels-pixabay-355321.jpg')";
+    document.getElementById("parent").style.background = "background-size: cover";
+};
+function changerSlideThree() {
+    document.body.style.backgroundImage = "url('./assets/pexels-max-ravier-2253821.jpg')";
+    document.getElementById("parent").style.backgroundImage = "linear-gradient(270deg, rgba(0,0,0,0.073564459963673) 31%, rgba(0, 0, 0, 0.247) 54%, rgba(0, 0, 0, 0.411) 100%),url('./assets/pexels-max-ravier-2253821.jpg')";
+    document.getElementById("parent").style.cssText = "background-size: cover";
+};
